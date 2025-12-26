4c3569697c0b9e01289fbceafc2d3ba242e42611
Anya-Kirshteyn <ann65ann@gmail.com>
2025-12-26 12:32:59 +0200

    lesson1/coursesTitleArray
    Signed-off-by: Anya-Kirshteyn <ann65ann@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,22 @@ import './App.css'
 
 function App() {
 
-
+ const coursesTitleArray:string[] = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+  ];
   return (
-    <>
-
-    </>
+    <ul>
+      {coursesTitleArray.map((course, i) => {
+        return (
+            <li className='coursesTitleArrayClass' key={i}>{course}</li>
+        )
+      })}
+    </ul>
   )
 }
 
