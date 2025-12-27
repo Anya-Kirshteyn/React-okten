b1ff7b74c39059965f6dc0bab119f3cf10448bbf
Anya-Kirshteyn <ann65ann@gmail.com>
2025-12-27 11:02:24 +0200

    lesson1/coursesAndDurationArray
    Signed-off-by: Anya-Kirshteyn <ann65ann@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 
 import './App.css'
+type coursesAndDurationType={title:string, monthDuration:number}
+const coursesAndDurationArray:coursesAndDurationType[] = [
+  {title: 'JavaScript Complex', monthDuration: 5},
+  {title: 'Java Complex', monthDuration: 6},
+  {title: 'Python Complex', monthDuration: 6},
+  {title: 'QA Complex', monthDuration: 4},
+  {title: 'FullStack', monthDuration: 7},
+  {title: 'Frontend', monthDuration: 4}
+];
 
 function App() {
 
 
   return (
     <>
-
+      {coursesAndDurationArray.map(course=>(
+          <div key={course.title}>
+            <h2>{course.title}</h2>
+            <p>{course.monthDuration }</p>
+          </div>
+      ))}
     </>
   )
 }
