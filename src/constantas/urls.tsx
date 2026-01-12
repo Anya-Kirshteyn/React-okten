8b27b6ec8dea5c286a1d1c8dc56535794f63a236
Anya-Kirshteyn <ann65ann@gmail.com>
2026-01-12 16:38:29 +0200

    routes users/posts/comments-bouth DummyJSON&JSONplaceholder
    Signed-off-by: Anya-Kirshteyn <ann65ann@gmail.com>

---
+++ src/constantas/urls.tsx
@@ -0,0 +1,23 @@
+const URLdummyJSON='https://dummyjson.com'
+const URLplaceholder='https://jsonplaceholder.typicode.com'
+
+export const URLS={
+    usersDummyJSON:{
+        allUsers:URLdummyJSON+'/users'
+    },
+    usersPlaceholder:{
+        allUsers:URLplaceholder+'/users'
+    },
+    postsDummyJSON:{
+        allPosts:URLdummyJSON+'/posts'
+    },
+    postsPlaceholder:{
+        allPosts:URLplaceholder+'/posts'
+    },
+    commentsDummyJSON:{
+        allComments:URLdummyJSON+'/comments'
+    },
+    commentsPlaceholder:{
+        allComments:URLplaceholder+'/comments'
+    }
+}
